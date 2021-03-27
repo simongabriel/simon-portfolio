66a1bd00a4db5c0481ebea23769d34bfb5a2967c
simongabriel <sgabriel1790@gmail.com>
2021-03-27 15:29:09 +0000

    adds fonts and things

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 // import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -22,7 +22,7 @@ const Layout = ({ children }) => {
       }
     }
   `)
-  
+
   return (
     <>
       <div
