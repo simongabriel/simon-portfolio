1532d34d4ac976af691d6a3c8f509842100782b3
simongabriel <sgabriel1790@gmail.com>
2021-03-27 14:53:50 +0000

    installs sass
    Installs sass, some basic changes such as removal of header

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+// import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -22,26 +22,21 @@ const Layout = ({ children }) => {
       }
     }
   `)
-
+  
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 896,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
+        <footer>
+          © {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="www.twitter.com/simongabriel">Simon Gabriel</a>
         </footer>
       </div>
     </>
