4dd4717ce35f6671837c785c37d96b1f37ddaf7e
simongabriel <sgabriel1790@gmail.com>
2021-03-27 15:39:03 +0000

    fonts and grid
    adds proper fonts and CSS grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,22 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Simon Gabriel" />
-    <h1>Hi, I'm Simon</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+  <SEO title="Simon Gabriel" />
+  <div class="grid-container">
+    <div class="left-col">
+      <h1>Hi, I'm Simon</h1>
+    </div>
+    <div class="right-col">
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </div>
+  </div>
+
+
   </Layout>
 )
 
