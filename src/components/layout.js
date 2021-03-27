aa407cf8b18d4e366a13d2f5c0bb4d4b074c1a10
simongabriel <sgabriel1790@gmail.com>
2021-03-27 17:32:05 +0000

    adds a lot of styling
    Gets me to a place I'm pretty happy

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 896,
+          maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,25 +11,43 @@ const IndexPage = () => (
 
   <div class="grid-container">
     <div class="avatar-container">
+    <StaticImage
+      src="../images/avatar.png"
+      style={{ borderRadius: 100, height: 136 }}
+      width={136}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+    />
+
+    <div class="list-container">
+      <ul class="list-1">
+        <li class="name">Simon Gabriel</li>
+        <li>he/him/his</li>
+        <li>@SimonGabriel</li>
+        <li>sgabriel1790@gmail.com</li>
+        <li>Download CV</li>
+      </ul>
+
+      <ul class="list-2">
+        <li>About</li>
+        <li>What I'm looking for</li>
+        <li>Work Samples</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+
     </div>
 
     <div class="header-container">
       <h1>Hi, I'm Simon</h1>
-      <p>I’m a UX/UI designer with a problem solving mindset who helps companies achieve user centric and frictionless experiences.</p>
+      <p class="intro-text">I’m a UX/UI designer with a problem solving mindset who helps companies achieve user centric and frictionless experiences.</p>
+      <div class="body-container">
+      <p>Body version; I’m a UX/UI designer with a problem solving mindset who helps companies achieve user centric and frictionless experiences.</p>
+      </div>
     </div>
 
-    <div class="body-container">
-    <p>Body version; I’m a UX/UI designer with a problem solving mindset who helps companies achieve user centric and frictionless experiences.</p>
-    </div>
 
-    <div class="list-container">
-    <p>This is the left col</p>
-      <ul>
-        <li>Coffee</li>
-        <li>Tea</li>
-        <li>Milk</li>
-      </ul>
-    </div>
 
   </div>
 
